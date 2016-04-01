81372f3401e41b59387fb496d37d584bd4b55829
extg <0777144@users.noreply.github.com>
2016-04-01 21:32:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,63 @@
 (function () {
-    function getJson(url){
-        return new Promise((resolve, reject) => {
-            var request = new XMLHttpRequest();
-            request.open('GET', url, true);
-
-            request.onload = function() {
-                if (request.status >= 200 && request.status < 400) {
-                    var data = JSON.parse(request.responseText);
-                    resolve(data);
-                } else {
-                    reject();
-                }
-            };
-
-            request.onerror = function() {
-                reject();
-            };
-
-            request.send();
-        });
+    
+    var cards = [
+  {
+    "from": "A",
+    "to": "B",
+    "ticket": {
+      "type": "bus",
+      "number": "848F",
+      "spec": "",
+      "seat": "98J"
     }
-
+  },
+  {
+    "from": "D",
+    "to": "B",
+    "ticket": {
+      "type": "airplane",
+      "number": "FZ83",
+      "spec": "",
+      "seat": "30C",
+      "gate": 35,
+      "baggage": "auto"
+    }
+  },
+  {
+    "from": "B",
+    "to": "E",
+    "ticket": {
+      "type": "airplane",
+      "number": "YN11",
+      "spec": "",
+      "seat": "81D",
+      "gate": 0,
+      "baggage": "auto"
+    }
+  },
+  {
+    "from": "C",
+    "to": "D",
+    "ticket": {
+      "type": "bus",
+      "number": "701C",
+      "spec": "",
+      "seat": "36R"
+    }
+  },
+  {
+    "from": "B",
+    "to": "C",
+    "ticket": {
+      "type": "airplane",
+      "number": "UF22",
+      "spec": "",
+      "seat": "16R",
+      "gate": 5,
+      "baggage": "auto"
+    }
+  }
+];
     Journey.fn.addTicketType("bus", function(card){
         let ticket = {
             number: card.ticket.number == "" ? "" : `${card.ticket.number} `,
@@ -31,19 +68,7 @@
     });
 
 
-    getJson('data/cardsABE.json')
-        .then(cards => {
-            var journey = new Journey(cards);
-            //var types = journey.getTypes(); // ["airplane", "train", "bus"]
-            //var instructions = journey.getInstructions(); // ["Take bus 848F from A to B. Seat 98J.",
-                                                            //  "From B, take flight UF22 to C. Gate 5. Seat undefi… be automatically transferred from your last leg.",
-                                                            //  "Take bus 701C from C to D. Seat 36R.",
-                                                            //  "From D, take flight FZ83 to B. Gate 35. Seat undef… be automatically transferred from your last leg.",
-                                                            //  "From B, take flight YN11 to E. Gate 0. Seat undefi… be automatically transferred from your last leg."]
-
-            //var simpleInstruction = journey.getSimple(); // ["From A to  B", "From B to  C", "From C to  D", "From D to  B", "From B to  E"]
-
-            journey.printInstructions("#instructions");
-            journey.printSimple("#simple");
-        });
+   var journey = new Journey(cards);
+    journey.printInstructions("#instructions");
+    journey.printSimple("#simple");
 })();
